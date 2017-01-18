70c43bf76af968b719a02644fbaec694fd370703
vishalgolcha <vishalgolcha@hotmail.com>
2017-01-18 22:00:14 +0530

    Removed keys

--- boo.py
+++
@@ -1,9 +0,0 @@
-import simplejson
-import httplib2
-import sys
-from instagram.client import InstagramAPI
-i=''
-sec=''
-api= InstagramAPI(client_id=i,client_secret=sec)
-popular_media=api.recent_media(count=1)
-print popular_media
